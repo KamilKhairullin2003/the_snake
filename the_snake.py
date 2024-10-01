c67e1f9e4011886aaefb738bd388e4f3a0a5a5c0
Kamil Khairullin <kamilkhairullin2003@gmail.com>
2024-10-01 17:41:28 +0300

    cdelal

--- the_snake.py
+++ the_snake.py
@@ -188,8 +188,9 @@ def handle_keys(snake: Snake):
     Параметр snake - объект змейки.
     """
     for event in pg.event.get():
-        if (event.type == pg.QUIT or
-             (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE)):
+        if event.type == pg.QUIT or (
+            event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE
+        ):
             pg.quit()
             raise SystemExit
         elif event.type == pg.KEYDOWN:
